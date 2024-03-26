9b3121c851a1b5a84e4330ccc4e131cb36d031d0
Thibaut Delille <tdelille@gmail.com>
2024-03-25 21:25:15 -0700

    console

--- client/src/config/firebase.ts
+++ client/src/config/firebase.ts
@@ -15,6 +15,8 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
+console.log(firebaseConfig);
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase
